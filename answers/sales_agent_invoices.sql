c19b6e02b2f3b50f0d2ea1726685cfcc877c2745
Dani Adkins <danirenaeadkins12@gmail.com>
2017-02-20 04:51:21 -0600

    completed 1to10

---
+++ answers/sales_agent_invoices.sql
@@ -0,0 +1,8 @@
+-- sales_agent_invoices.sql: Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agent's full name.
+
+
+SELECT Invoice.
+FROM Employee, Invoice
+WHERE
+
+
